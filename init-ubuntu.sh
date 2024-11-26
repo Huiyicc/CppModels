df687dbd871a5df81e94c6b42b21d5ffea9a80e8
Huiyicc <1925473403@qq.com>
2024-11-27 00:04:23 +0800

    修改ubuntu的初始化脚本

--- init-ubuntu.sh
+++ init-ubuntu.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sudo apt install autoconf autogen automake build-essential libasound2-dev \
+  libflac-dev libogg-dev libtool libvorbis-dev libopus-dev libmp3lame-dev \
+  libmpg123-dev pkg-config python
 git clone https://github.com/Huiyicc/cpp-pinyin.git
 git clone -b 00bb81de769a80303e607aea149419902230bc4b https://github.com/huiyicc/json
 git clone -b 78cc5c173404488d80751af226d1eaf67033bcc4 https://github.com/libsdl-org/SDL
@@ -17,3 +20,4 @@ git clone https://github.com/huiyicc/SRELL
 git clone https://github.com/Huiyicc/utfcpp
 git clone https://github.com/Huiyicc/cppjieba
 git clone https://github.com/Huiyicc/libsamplerate
+git clone https://github.com/Huiyicc/cld2-cmake
